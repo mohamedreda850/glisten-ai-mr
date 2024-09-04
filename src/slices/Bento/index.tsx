5af9bd825a701ecd6513f362fafb29c58fce3132
Mohamed Reda <mohamed.reda86060@gmail.com>
2024-09-05 01:30:08 +0300

    new

--- src/slices/Bento/index.tsx
+++ src/slices/Bento/index.tsx
@@ -41,8 +41,8 @@ const Bento = ({ slice }: BentoProps): JSX.Element => {
         <PrismicRichText field={slice.primary.body} />
       </div>
       <div className="mt-16 grid max-w-4xl grid-rows-[auto_auto_auto] gap-8 md:grid-cols-3 md:gap-10">
-        {slice.primary.repetable_zone.map((item) => (
-          <div
+        {slice.primary.repetable_zone.map((item , index) => (
+          <div key={index}
             className={clsx(
               "glass-container row-span-3 grid grid-rows-subgrid gap-4 rounded-lg bg-gradient-to-b from-gray-900 to-gray-950 p-4",
               item.wide ? "md:col-span-2" : "md:col-span-1",
